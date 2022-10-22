dc6976e1e31d13dab8d0e45f129c24252978a2b8
jdpuertas <j.diegopuertas@gmail.com>
2022-10-22 11:35:43 -0500

    cambio en la base de datos

--- src/main/java/com/proyectointegrador/proyectointegrador/Model/Empleado.java
+++ src/main/java/com/proyectointegrador/proyectointegrador/Model/Empleado.java
@@ -4,6 +4,7 @@ import com.proyectointegrador.proyectointegrador.Enums.Enum_Rol;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
