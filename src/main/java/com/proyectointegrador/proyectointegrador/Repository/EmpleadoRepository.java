974024b6e6db37eb1401067f6d8d2eec48a4a6c9
unknown <jarroarb@crystal.com.co>
2022-10-23 11:15:17 -0500

    Se agregó la etiqueta Repository

--- src/main/java/com/proyectointegrador/proyectointegrador/Repository/EmpleadoRepository.java
+++ src/main/java/com/proyectointegrador/proyectointegrador/Repository/EmpleadoRepository.java
@@ -1,6 +1,9 @@
 package com.proyectointegrador.proyectointegrador.Repository;
 import com.proyectointegrador.proyectointegrador.Model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
 }
